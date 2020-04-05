0e772f8295030b113e5bf3d0efcd8374d9620b33
Flowmaster <ellyssin.gimhae@gmail.com>
2020-04-05 17:19:58 -0600

    Added width and height getters for Universe.

--- src/lib.rs
+++ src/lib.rs
@@ -66,6 +66,18 @@ impl Universe {
         self.to_string()
     }
 
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+
     fn get_index(&self, row: u32, column: u32) -> usize {
         (row * self.width + column) as usize
     }
