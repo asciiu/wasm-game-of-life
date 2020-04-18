74ca7da987e1b75175a1ac7ec7e70f2cd29eb170
Flowmaster <ellyssin.gimhae@gmail.com>
2020-04-17 22:22:19 -0600

    Set doge near top of screen.

--- web/index.js
+++ web/index.js
@@ -12,32 +12,79 @@ app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 app.renderer.autoResize = true
 
- 
-const container = new PIXI.Container();
-app.stage.addChild(container);
+const doge = PIXI.Sprite.from('images/doge.png');
 
-const texture = PIXI.Texture.from('images/doge.png');
+// set the anchor point so the texture is centerd on the sprite
+doge.anchor.set(0.5);
+doge.x = app.screen.width / 2;
+doge.y = app.screen.height / 6;
+doge.scale.set(0.3);
+app.stage.addChild(doge);
 
-// Create a 5x5 grid of bunnies
-for (let i = 0; i < 25; i++) {
-    const bunny = new PIXI.Sprite(texture);
-    bunny.anchor.set(0.5);
-    bunny.x = (i % 5) * 40;
-    bunny.y = Math.floor(i / 5) * 40;
-    container.addChild(bunny);
-}
+// holder to store the aliens
+//const aliens = [];
+//
+//const totalDudes = 20;
+//
+//for (let i = 0; i < totalDudes; i++) {
+//    // create a new Sprite that uses the image name that we just generated as its source
+//    const dude = PIXI.Sprite.from('images/doge.png');
+//
+//    // set the anchor point so the texture is centerd on the sprite
+//    dude.anchor.set(0.5);
+//
+//    // set a random scale for the dude - no point them all being the same size!
+//    dude.scale.set(0.1 + Math.random() * 0.3);
+//
+//    // finally lets set the dude to be at a random position..
+//    dude.x = Math.random() * app.screen.width;
+//    dude.y = Math.random() * app.screen.height;
+//
+//    dude.tint = Math.random() * 0xFFFFFF;
+//
+//    // create some extra properties that will control movement :
+//    // create a random direction in radians. This is a number between 0 and PI*2 which is the equivalent of 0 - 360 degrees
+//    dude.direction = Math.random() * Math.PI * 2;
+//
+//    // this number will be used to modify the direction of the dude over time
+//    dude.turningSpeed = Math.random() - 0.8;
+//
+//    // create a random speed for the dude between 2 - 4
+//    dude.speed = 2 + Math.random() * 2;
+//
+//    // finally we push the dude into the aliens array so it it can be easily accessed later
+//    aliens.push(dude);
+//
+//    app.stage.addChild(dude);
+//}
 
-// Move container to the center
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
-
-// Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
-
-// Listen for animate update
-app.ticker.add((delta) => {
-    // rotate the container!
-    // use delta to create frame-independent transform
-    container.rotation -= 0.01 * delta;
-});
+// create a bounding box for the little dudes
+//const dudeBoundsPadding = 100;
+//const dudeBounds = new PIXI.Rectangle(-dudeBoundsPadding,
+//    -dudeBoundsPadding,
+//    app.screen.width + dudeBoundsPadding * 2,
+//    app.screen.height + dudeBoundsPadding * 2);
+//
+//app.ticker.add(() => {
+//    // iterate through the dudes and update their position
+//    for (let i = 0; i < aliens.length; i++) {
+//        const dude = aliens[i];
+//        dude.direction += dude.turningSpeed * 0.01;
+//        dude.x += Math.sin(dude.direction) * dude.speed;
+//        dude.y += Math.cos(dude.direction) * dude.speed;
+//        dude.rotation = -dude.direction - Math.PI / 2;
+//
+//        // wrap the dudes by testing their bounds...
+//        if (dude.x < dudeBounds.x) {
+//            dude.x += dudeBounds.width;
+//        } else if (dude.x > dudeBounds.x + dudeBounds.width) {
+//            dude.x -= dudeBounds.width;
+//        }
+//
+//        if (dude.y < dudeBounds.y) {
+//            dude.y += dudeBounds.height;
+//        } else if (dude.y > dudeBounds.y + dudeBounds.height) {
+//            dude.y -= dudeBounds.height;
+//        }
+//    }
+//}); 
