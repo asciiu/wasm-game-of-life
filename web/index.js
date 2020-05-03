33dbe9b97d92d80559021fdac30cfe83c30518ce
Flowmaster <ellyssin.gimhae@gmail.com>
2020-05-02 18:32:14 -0600

    Added btc coin flip.

--- web/index.js
+++ web/index.js
@@ -1,26 +1,39 @@
 import * as PIXI from 'pixi.js'
 import * as Keyboard from 'pixi.js-keyboard'
-import * as Mouse from 'pixi.js-mouse'
 import { Ship } from './ship.js'
 
 // The application will create a renderer using WebGL, if possible,
 // with a fallback to a canvas render. It will also setup the ticker
 // and the root stage PIXI.Container.
 const app = new PIXI.Application({
-  width: 800, 
-  height: 600, 
-  backgroundColor: 0x061639, 
-  resolution: window.devicePixelRatio || 1,
+  width: 800,
+  height: 800,
+  backgroundColor: 0x061639,
+  //resolution: window.devicePixelRatio || 1,
+  resolution: 1,
 });
 document.body.appendChild(app.view);
 
 const player = new Ship({
   clientID: 1,
   app: app,
-  image: "images/rocket.png", 
+  image: "images/rocket.png",
   x: app.screen.width / 2,
   y: app.screen.height / 2,
 })
+
+const btc = PIXI.Sprite.from('images/btc.png');
+btc.anchor.set(0.5);
+//btc.x = app.screen.width / 2;
+//btc.y = app.screen.height / 2;
+//btc.scale.x = 0.1;
+//btc.scale.y = 0.1;
+btc.width = 40;
+btc.height = 40; 
+const container = new PIXI.Container();
+container.addChild(btc);
+container.x = app.screen.width / 2;
+container.y = app.screen.height / 2;
 
 const starTexture = PIXI.Texture.from('images/star.png');
 
@@ -36,55 +49,70 @@ const starBaseSize = 0.05;
 const stars = [];
 const torpedos = [];
 for (let i = 0; i < starAmount; i++) {
-    const star = {
-        sprite: new PIXI.Sprite(starTexture),
-        z: 0,
-        x: 0,
-        y: 0,
-    };
-    star.sprite.anchor.x = 0.5;
-    star.sprite.anchor.y = 0.7;
-    star.sprite.alpha = 0.7;
-    star.sprite.tint = Math.random() * 0xFFFFFF;
-    randomizeStar(star, true);
-    app.stage.addChild(star.sprite);
-    stars.push(star);
+  const star = {
+    sprite: new PIXI.Sprite(starTexture),
+    z: 0,
+    x: 0,
+    y: 0,
+  };
+  star.sprite.anchor.x = 0.5;
+  star.sprite.anchor.y = 0.7;
+  star.sprite.alpha = 0.7;
+  star.sprite.tint = Math.random() * 0xFFFFFF;
+  randomizeStar(star, true);
+  app.stage.addChild(star.sprite);
+  stars.push(star);
 }
 
 function randomizeStar(star, initial) {
-    star.z = initial ? Math.random() * 2000 : cameraZ + Math.random() * 1000 + 2000;
+  star.z = initial ? Math.random() * 2000 : cameraZ + Math.random() * 1000 + 2000;
 
-    // Calculate star positions with radial random coordinate so no star hits the camera.
-    const deg = Math.random() * Math.PI * 2;
-    const distance = Math.random() * 50 + 1;
-    star.x = Math.cos(deg) * distance;
-    star.y = Math.sin(deg) * distance;
+  // Calculate star positions with radial random coordinate so no star hits the camera.
+  const deg = Math.random() * Math.PI * 2;
+  const distance = Math.random() * 50 + 1;
+  star.x = Math.cos(deg) * distance;
+  star.y = Math.sin(deg) * distance;
 }
 
 setup();
 
 function setup() {
+  app.stage.addChild(container);
   app.stage.addChild(player.container);
 
   //Start the game loop 
   app.ticker.add(delta => gameLoop(delta));
 }
 
-function gameLoop(delta){
+let flip = true;
+
+function gameLoop(delta) {
   input(delta);
   player.render(delta);
+
+  if (container.scale.x > -1 && flip) {
+    container.scale.x -= 0.05;
+    if (container.scale.x <= -1) {
+      flip = false;
+    }
+  } else if (container.scale.x < 1 && !flip) {
+    container.scale.x += 0.05;
+    if (container.scale.x >= 1) {
+      flip = true;
+    }
+  }
 
   // Simple easing. This should be changed to proper easing function when used for real.
   speed += (warpSpeed - speed) / 20;
   cameraZ += delta * 10 * (speed + baseSpeed);
-  stars.forEach(function(star) {
+  stars.forEach(function (star) {
     if (star.z < cameraZ) randomizeStar(star);
-  
+
     // Map star 3d position to 2d with really simple projection
     const z = star.z - cameraZ;
     star.sprite.x = star.x * (fov / z) * app.renderer.screen.width + app.renderer.screen.width / 2;
     star.sprite.y = star.y * (fov / z) * app.renderer.screen.width + app.renderer.screen.height / 2;
-  
+
     // Calculate star scale & rotation.
     const dxCenter = star.sprite.x - app.renderer.screen.width / 2;
     const dyCenter = star.sprite.y - app.renderer.screen.height / 2;
@@ -97,7 +125,7 @@ function gameLoop(delta){
     star.sprite.rotation = Math.atan2(dyCenter, dxCenter) + Math.PI / 2;
   })
 
-  torpedos.forEach(function(torpedo) {
+  torpedos.forEach(function (torpedo) {
     torpedo.render();
     if (torpedo.offScreen(app.screen.width, app.screen.height)) {
       app.stage.removeChild(torpedo.sprite);
@@ -123,7 +151,7 @@ function input(delta) {
   }
   if (Keyboard.isKeyDown('Space')) {
     warpSpeed = 1;
-    setTimeout( function() {
+    setTimeout(function () {
       warpSpeed = 0;
     }, 5000);
   }
